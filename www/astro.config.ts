eb2479a5b5d22810c36de05af3af8a0bb48c9f8a
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-05-07 21:59:11 +0100

    tw

--- www/astro.config.ts
+++ www/astro.config.ts
@@ -1,7 +1,11 @@
 import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
