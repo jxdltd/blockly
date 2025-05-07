d97519660c5fe7c5718f3181d27eb0e0dd020105
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-05-07 22:59:04 +0100

    www

--- www/astro.config.ts
+++ www/astro.config.ts
@@ -1,11 +1,20 @@
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: 'Pacifico',
+        cssVariable: '--font-logo',
+      },
+    ],
   },
 });
