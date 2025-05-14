52a1eb23f6362b561a21e40f33f806d17d0c284b
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-05-14 18:01:01 +0100

    sitemap

--- www/astro.config.ts
+++ www/astro.config.ts
@@ -1,9 +1,12 @@
+import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://blockly.jxd.dev',
+  integrations: [sitemap()],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
